84df5b61bb3016c2e5c65fdf00aabfb14315830f
andrenaP <a0993100106@gmail.com>
2023-06-02 19:13:53 +0300

    did 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const button_start = document.querySelector('[data-start]');
+const button_stop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+const Color_body=()=>{ body.style.background= getRandomHexColor() }
+button_start.addEventListener('click', () => {timerId = setInterval(Color_body, 1e3); })
+button_stop.addEventListener('click', () => { if(timerId) clearInterval(timerId); })
